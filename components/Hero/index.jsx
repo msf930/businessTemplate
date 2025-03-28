a1e8a9f248daea815ff93551897f760e2178e2c3
Matt Folsom <mattfolsom@MacBook-Pro-6.local>
2025-03-27 23:29:04 -0600

    about parallax

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -8,6 +8,7 @@ import Hero2 from "@/public/DefaultAbout.jpg";
 import {motion, useScroll, useTransform} from "motion/react";
 
 import {client} from "@/sanity/lib/client";
+import styles from "@/components/Hero2/styles.module.css";
 
 const ABOUT_QUERY = `*[_type == "aboutPage"] {_id, heroImage { asset -> { _id, url } }}`;
 const options = { next: { revalidate: 30 } };
@@ -19,7 +20,7 @@ const Index = () => {
         target: ref,
         offset: ["start end", "end start"],
     });
-    const backgroundY = useTransform(scrollYProgress, [0, 1], ["-50%", "100%"]);
+    const backgroundY = useTransform(scrollYProgress, [0, 1], ["-80%", "-20%"]);
 
     //data fetching
     const [data, setData] = useState([]);
@@ -38,16 +39,23 @@ const Index = () => {
 
     return (
         <>
-            <motion.div className="heroContainer2-about" style={{y: backgroundY}}>
-                <Image
-                    src={data[0]?.heroImage?.asset?.url || Hero2}
-                    alt="Hero"
-                    id="HeroImg"
-                    fill
-                    objectFit="cover"
-                />
-                {/*<div className="rectangular-frame-about" >*/}
-                {/*</div>*/}
+            <motion.div
+                className="aboutHeroContainer"
+                ref={ref}
+            >
+                <motion.div className="aboutHeroParallax" style={{y: backgroundY}}>
+                    <motion.div className="heroContainer2-about" >
+                        <Image
+                            src={data[0]?.heroImage?.asset?.url || Hero2}
+                            alt="Hero"
+                            id="HeroImg"
+                            fill
+                            objectFit="cover"
+                        />
+                        {/*<div className="rectangular-frame-about" >*/}
+                        {/*</div>*/}
+                    </motion.div>
+                </motion.div>
             </motion.div>
             <div className="heroContainer2Sm-about">
                 <Image
